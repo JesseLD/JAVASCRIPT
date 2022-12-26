148dd67e9bfd1c3c01d3c091c5e48d45f84b8b03
Jessé Oliveira <82299456+JesseLD@users.noreply.github.com>
2022-12-26 08:36:02 -0300

    Update

--- exercicios/aula15/js/index.js
+++ exercicios/aula15/js/index.js
@@ -1,11 +1,9 @@
 document.querySelector(".button").addEventListener('click',handleClick);
-
+const date = new Date().getFullYear();
 
 function handleClick(){
-  let age = document.querySelector("#age"); 
-  let gender = document.querySelectorAll("input.sex") 
-  let date = new Date().getFullYear();
-
+  const age = document.querySelector("#age"); 
+  const gender = document.querySelectorAll("input.sex") 
   // sanitizeNumberInput(age);
   // getAge(sanitizeNumberInput(age));
   
@@ -15,26 +13,21 @@ function handleClick(){
     if(age.value > date){
       alert("Data Inválida!")
     }else{
-      verifyPerson(getAge(sanitizeNumberInput(age)),getSelectedGender(gender));
       getPeople(getSelectedGender(gender),getAge(sanitizeNumberInput(age)));
     }
   }
-  
-
 
 }
 
 function sanitizeNumberInput(age){
-  let newAge = Number(age.value);
-  let checkAge = newAge;
+  const newAge = Number(age.value);
   return newAge;
   
 }
 
 function getSelectedGender(gender){
-
   let genders = ['Um Homem','Uma Mulher']
-  for(i=0;i<gender.length;i++){
+  for(let i=0;i<gender.length;i++){
     if(gender[i].checked){
       let index = i;
       // console.log(genders[index]);
@@ -45,10 +38,8 @@ function getSelectedGender(gender){
 
 }
 function getAge(age){
-  let date = new Date().getFullYear();
     newAge = age;
     return date - newAge;
-  
   
 }
 function getPeople(gender,age){
@@ -56,65 +47,87 @@ function getPeople(gender,age){
     newAge = age;
 
     let html = '<div class="html"></div>';
-    html += `<img src=>`
+    html += verifyPerson(age,gender,html);
     html += `Detectamos ${genders} com ${newAge} anos`
     document.querySelector('.html').innerHTML = html;
   
-
 }
 
-function verifyPerson(age,gender){
+function verifyPerson(age,gender,html){
+  let image
 
   if(age >= 0 && age < 11){
     console.log("Criança")
     if(gender == "Um Homem"){
-      console.log("Homem")
+      console.log("Homem");
+      image = "https://images.pexels.com/photos/7105798/pexels-photo-7105798.jpeg?auto=compress&cs=tinysrgb&w=600";
     }else{
-      console.log("Mulher")
+      console.log("Mulher");
+      image = "https://images.pexels.com/photos/14752730/pexels-photo-14752730.jpeg?auto=compress&cs=tinysrgb&w=600";
     }
   }
 
   if(age >= 11 && age < 18){
     console.log("Jovem")
     if(gender == "Um Homem"){
-      console.log("Homem")
+      console.log("Homem");
+      image = "https://images.pexels.com/photos/9353444/pexels-photo-9353444.jpeg?auto=compress&cs=tinysrgb&w=600";
     }else{
-      console.log("Mulher")
+      console.log("Mulher");
+      image = "https://images.pexels.com/photos/1462630/pexels-photo-1462630.jpeg?auto=compress&cs=tinysrgb&w=600";
     }
   }
+
   if(age >= 18 && age < 30){
     console.log("Jovem Adulto")
     if(gender == "Um Homem"){
-      console.log("Homem")
+      console.log("Homem");
+      image = "https://images.pexels.com/photos/14711370/pexels-photo-14711370.jpeg?auto=compress&cs=tinysrgb&w=600";
     }else{
-      console.log("Mulher")
+      console.log("Mulher");
+      image = "https://images.pexels.com/photos/3769021/pexels-photo-3769021.jpeg?auto=compress&cs=tinysrgb&w=600";
     }
   }
+
   if(age >= 30 && age < 60){
     console.log("Adulto")
     if(gender == "Um Homem"){
-      console.log("Homem")
+      console.log("Homem");
+      image = "https://images.pexels.com/photos/4586676/pexels-photo-4586676.jpeg?auto=compress&cs=tinysrgb&w=600";
     }else{
-      console.log("Mulher")
+      console.log("Mulher");
+      image = "https://images.pexels.com/photos/11179656/pexels-photo-11179656.jpeg?auto=compress&cs=tinysrgb&w=600";
     }
   }
-  if(age >= 60){
+
+  if(age >= 60 && age < 120){
     console.log("Idoso")
     if(gender == "Um Homem"){
-      console.log("Homem")
+      console.log("Homem");
+      image = "https://images.pexels.com/photos/1139743/pexels-photo-1139743.jpeg?auto=compress&cs=tinysrgb&w=600";
     }else{
-      console.log("Mulher")
+      console.log("Mulher");
+      image = "https://images.pexels.com/photos/2050994/pexels-photo-2050994.jpeg?auto=compress&cs=tinysrgb&w=600";
     }
   }
-  drawImage();
+  
+  if(age >= 120){
+    console.log("Morto")
+    if(gender == "Um Homem"){
+      console.log("Morto");
+      image = "https://images.pexels.com/photos/3756616/pexels-photo-3756616.jpeg?auto=compress&cs=tinysrgb&w=600";
+    }else{
+      console.log("Morto");
+      image = "https://images.pexels.com/photos/9740352/pexels-photo-9740352.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+    }
+  }
+  
+  return html += `<img class="img" src=${image}><br>`
+  
   
 
 }
 
-function drawImage(){
-  
-
-}
 
 // handleClick()
 
